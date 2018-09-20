839a1d737c0afed516a0d0025b655b53b9865aae
Michael Insalaco <insalaco01@gmail.com>
2018-09-20 16:41:00 -0400

    add location

--- ui.js
+++ ui.js
@@ -1,10 +1,12 @@
 class UI {
   constructor() {
+    this.location = document.getElementById('w-location');
     this.temp = document.getElementById('w-temp');
     this.humidity = document.getElementById('w-humidity');
   }
 
   paint(weather) {
+    this.location.textContent = weather.name;
     this.humidity.textContent = 'humidity: ' + weather.main.humidity;
     this.temp.textContent = 'temp: ' + weather.main.temp
   }
