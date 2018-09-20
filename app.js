862886ce575ec85bdd03093c79bb0048d7cd2c5f
Michael Insalaco <insalaco01@gmail.com>
2018-09-20 16:38:28 -0400

    instantiate and call UI method

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 // init weather object
 const weather = new Weather('17403');
+const ui = new UI();
 
-// call the getWeather method from the Weather class
-// returns promise so need to use .then()
-weather.getWeather()
+// when the DOM loads call getWeather function
+document.addEventListener('DOMContentLoaded', getWeather);
+
+function getWeather() {
+  // call the getWeather method in the Weather class
+  // returns promise because of async so need to use .then()
+  weather.getWeather()
   .then(results => {
     console.log(results);
+    ui.paint(results);
   })
   .catch(err => console.log(err));
+}
