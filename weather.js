c5703e7e34842792a6445842ca805aee70b78f4e
Michael Insalaco <insalaco01@gmail.com>
2018-09-20 16:40:15 -0400

    rename zip to zipcode

--- weather.js
+++ weather.js
@@ -1,17 +1,17 @@
 class Weather {
-  constructor(zip) {
+  constructor(zipcode) {
     this.apiKey = '';
-    this.zip = zip;
+    this.zipcode = zipcode;
   }
 
   // fetch api
   async getWeather() {
     // assign variable 'response' equal to api url
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${this.zip},us&appid=${this.apiKey}`);
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${this.zipcode},us&appid=${this.apiKey}`);
 
     const responseData = await response.json();
     
-    // returns weather object from resonse data
+    // returns weather object in JSON from resonseData above
     return responseData;
 
   }
