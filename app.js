0b8945fd8e8fa4ee24c0f24e0c661d11e8df1df7
Michael Insalaco <insalaco01@gmail.com>
2018-09-21 14:09:03 -0400

    instantiate local storage and create change location

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
+// init storage
+const storage = new Storage();
+
+// get stored location data
+const weatherLocation = storage.getLocationData();
+
 // init weather object
-const weather = new Weather('17403');
+const weather = new Weather(weatherLocation.zipcode);
+
+// init ui object
 const ui = new UI();
 
 // when the DOM loads call getWeather function
 document.addEventListener('DOMContentLoaded', getWeather);
 
+// change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+  const zipcode = document.getElementById('zip').value;
+ 
+  weather.changeLocation(zipcode);
+  
+  // get and display weather  
+  storage.setLocationData(zipcode);
+
+  // call getWeather function below
+  getWeather();
+
+  // close modal using jQuery
+  $('#locModal').modal('hide');
+});
+
+
 function getWeather() {
-  // call the getWeather method in the Weather class
+  // calls the getWeather method in the Weather class
   // returns promise because of async so need to use .then()
   weather.getWeather()
   .then(results => {
-    console.log(results);
     ui.paint(results);
   })
   .catch(err => console.log(err));
